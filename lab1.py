ac62e178bc1d7f4c1206aaa2a7b0d85bdcc2bc2e
peijen <peijen@ualberta.ca>
2016-09-07 15:16:08 -0600

    made a quine

--- lab1.py
+++ lab1.py
@@ -1,3 +1,3 @@
 import requests
-response = requests.post('http://ccid-eddieantonio.rhcloud.com/peijen')
-print(response.status_code)
+response = requests.get('https://github.com/peijen/cmput404-Lab1/raw/master/lab1.py')
+print(response.text)
